1fefc567ff68e63f876eea4b7b17117757649612
Rafael Ortiz <rd.ortizr1@uniandes.edu.co>
2022-08-29 22:06:04 -0500

    update

--- src/test.ts
+++ src/test.ts
@@ -20,6 +20,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec_old\.ts$/);
+const context = require.context('./', true, /\app.component..spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
